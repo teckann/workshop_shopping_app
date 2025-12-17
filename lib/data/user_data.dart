40ae4eae0e1a26c21ae453afa5ec2902bbc88df4
Gan Teck Ann <ganteckann582006@gmail.com>
2025-12-17 14:02:32 +0800

    update

--- lib/data/user_data.dart
+++ lib/data/user_data.dart
@@ -1,10 +1,12 @@
 import '../models/user.dart';
 
-final User user = User(
-  name: "Gan Teck Ann",
-  email: "TP083567@mail.apu.edu.my",
-
+User user = User(
+  name: 'user',
+  email: 'user@example.com',
+  phoneNumber: '123-456-7890',
   photoUrl:
-      "https://i.pinimg.com/236x/60/c1/da/60c1da97030b890ca4d8397f7557f285.jpg",
-  address: "Bukit Jalil, Kuala Lumpur",
+      'https://i.pinimg.com/236x/60/c1/da/60c1da97030b890ca4d8397f7557f285.jpg',
+  address:
+      '1, Jalan Imbi, Imbi, 55100 Kuala Lumpur, Federal Territory of Kuala Lumpur',
+  createdAt: DateTime.now(),
 );
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,13 +1,17 @@
 class User {
   final String name;
   final String email;
+  final String phoneNumber;
   final String photoUrl;
   final String address;
+  final DateTime createdAt;
 
   User({
     required this.name,
     required this.email,
+    required this.phoneNumber,
     required this.photoUrl,
     required this.address,
+    required this.createdAt,
   });
 }
